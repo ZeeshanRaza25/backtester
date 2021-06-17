8dfe9e3d1b0aa0d35b2f785963d1ca051fafad19
“Zeeshan <zeeshanraza252627@gmail.com>
2021-06-17 20:01:46 +0500

    update login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import * as firebaseui from "firebaseui";
 import firebase from "firebase/app";
 import "firebase/auth";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Button, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      marginTop: theme.spacing(4),
+      width: 200,
+    },
+  },
+}));
+
 
 const Login: React.FC = () => {
   React.useEffect(() => {
@@ -18,10 +32,48 @@ const Login: React.FC = () => {
     });
     authUI.start("#firebaseui-auth-container", uiConfig)
   });
+  const classes = useStyles();
 
   return (
-    <div id="firebaseui-auth-container" />
+    <div
+    //  id="firebaseui-auth-container"
+    >
+      {/* login page */}
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        spacing={1}
+        className={classes.root}
+      >
+        <Grid item xs>
+          <TextField
+            label="Email"
+            id="outlined-size-small"
+            defaultValue=""
+            variant="outlined"
+            size="small"
+          />
+        </Grid>
+        <Grid item xs>
+          <TextField
+            label="Password"
+            id="outlined-size-normal"
+            defaultValue=""
+            variant="outlined"
+            type="password"
+          />
+        </Grid>
+        <Grid item xs>
+          <Button variant="contained" color="primary">
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+
+    </div>
   );
 };
 
-export {Login};
+export { Login };
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,6 +2,13 @@ import firebase from "firebase/app";
 
 const firebaseConfig = {
   // please create a web app on Firebase Console to get the configuration (https://console.firebase.google.com)
+  apiKey: "AIzaSyAy7njTMTITaJE4DdW7RL1DHLKxgpvp9tw",
+  authDomain: "react-crud-f940e.firebaseapp.com",
+  databaseURL: "https://react-crud-f940e-default-rtdb.firebaseio.com",
+  projectId: "react-crud-f940e",
+  storageBucket: "react-crud-f940e.appspot.com",
+  messagingSenderId: "342707273595",
+  appId: "1:342707273595:web:d530da15ab29c0be8687de"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
